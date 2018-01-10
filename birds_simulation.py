80d192da59c14dee8be6f6abefb132b05ba637ef
Zuzanna Rec <zuzanna.rec@gmail.com>
2018-01-10 19:42:05 +0100

    Another species added

--- birds_simulation.py
+++ birds_simulation.py
@@ -9,84 +9,85 @@ size = [width, height]
 screen = pygame.display.set_mode(size)
 # Make the mouse pointer invisible one the screen
 pygame.mouse.set_visible(0)
+bg = pygame.image.load("sky.jpg")
+bg = pygame.transform.scale(bg, (width, height))
 
 center_x = width / 2
 center_y = height / 2
 
-barriers = [[500, 120], [500, 140], [500, 160], [500, 180], [500, 200], [500, 220], [600, 320], [600, 340], [600, 360], [600, 380], [600, 400], [600, 420], [800, 620], [800, 640], [800, 660], [800, 680], [800, 600], [800, 620]]
+barriers = [[500, 160], [500, 180], [500, 200], [640, 360], [640, 380], [640, 400], [800, 640], [800, 660], [800, 680]]
 barrier_radius = 15
 
-no_of_birds = 120
+no_of_birds = 60
+no_of_birds2 = 20
 position_spread = 800
 speed_spread = 2
 max_speed = 4
+max_speed2 = 10
 
 border = 100
 leader_border = 200
 border_speed_change = 0.2
+border2 = 200
+leader_border2 = 400
+border_speed_change2 = 0.4
 
+min_dist_between_species = 30.0
 min_dist = 15.0
 match_speed_window = 70.0
+min_dist2 = 20.0
+match_speed_window2 = 70.0
 
 leader_random_speed_change = 0.2
 leader_max_speed = 4.0
+leader_random_speed_change2 = 0.4
+leader_max_speed2 = 10.0
 
 birdlist = []
+birdlist2 = []
 food = None
 predator = None
 
 # Generate leader bird
-leaderbirdx = 300.0
+leaderbirdx = 800.0
 leaderbirdy = 300.0
 leaderbirdvx = 4.0
-leaderbirdvy = 0.0
+leaderbirdvy = 4.0
+leaderbirdx2 = 600.0
+leaderbirdy2 = 600.0
+leaderbirdvx2 = -4.0
+leaderbirdvy2 = 0.0
 
 # Generate birds
 i = 0
+k = 0
 food_closest = None
 predator_farthest = None
-
-bg = pygame.image.load("sky.jpg")
-bg = pygame.transform.scale(bg, (width, height))
+food_closest2 = None
 
 while (i < no_of_birds):
-    x = random.uniform(center_x - position_spread, center_x + position_spread)
-    y = random.uniform(center_y - position_spread, center_y + position_spread)
+    x = random.uniform(center_x, width)
+    y = random.uniform(0, height)
     vx = random.uniform(-speed_spread, speed_spread)
     vy = random.uniform(-speed_spread, speed_spread)
     added_bird = [x, y, vx, vy]
     birdlist.append(added_bird)
     i += 1
-
-
-def calculate_av_speed_of_nearby(vx, vy):
-    j = 0
-    avxtotal = 0
-    avytotal = 0
-    avcount = 0
-    while (j < no_of_birds):
-        if (j != i):
-            dx = birdlist[j][0] - x  # x-distance between current i-th bird and j-th bird
-            dy = birdlist[j][1] - y  # y-distance between current i-th bird and j-th bird
-            dist = math.sqrt(dx * dx + dy * dy)  # euclidean distance
-            # if distance is smaller than minimum distance birds move away from each other
-            if (dist < min_dist):
-                vx -= dx * 0.3
-                vy -= dy * 0.3
-            # sum up velocities of nearby birds and counts them
-            if (dist < match_speed_window):
-                avxtotal += birdlist[j][2]
-                avytotal += birdlist[j][3]
-                avcount += 1
-        j += 1
-    return avxtotal, avytotal, avcount, vx, vy
+while (k < no_of_birds2):
+    x2 = random.uniform(0, center_x)
+    y2 = random.uniform(0, height)
+    vx2 = random.uniform(-speed_spread, speed_spread)
+    vy2 = random.uniform(-speed_spread, speed_spread)
+    added_bird2 = [x2, y2, vx2, vy2]
+    birdlist2.append(added_bird2)
+    k += 1
 
 quit_pressed = False
 iterator = 0
 while not quit_pressed:
     # Set screen background
-    # screen.fill((102, 153, 255))
-    screen.blit(bg, (0, 0))
+    screen.fill((102, 153, 255))
+    #screen.blit(bg, (0, 0))
     iterator += 1
 
 # -------------------------------------------------------------------------------------------
@@ -107,7 +108,7 @@ while not quit_pressed:
     # Randomly place food on the screen
     if food is not None:
         pygame.draw.circle(screen, (0, 100, 0), food, 12)
-        if min_food_dist < 10.0:
+        if min_food_dist < 10.0 or min_food_dist2 < 20.0:
             food = None
 
 # -------------------------------------------------------------------------------------------
@@ -204,7 +205,6 @@ while not quit_pressed:
             if food_dist < min_food_dist:
                 min_food_dist = food_dist
                 food_closest = i
-
         # Birds calculate distance from predator
         if predator is not None:
             predator_dist = math.sqrt(math.pow(x - predator[0], 2) + math.pow(y - predator[1], 2))
@@ -214,7 +214,25 @@ while not quit_pressed:
 
         # Calculate average velocity of other birds
         # Bird moves away from neighbours that are too close
-        avxtotal, avytotal, avcount, vx, vy = calculate_av_speed_of_nearby(vx, vy)
+        j = 0
+        avxtotal = 0
+        avytotal = 0
+        avcount = 0
+        while (j < no_of_birds):
+            if (j != i):
+                dx = birdlist[j][0] - x  # x-distance between current i-th bird and j-th bird
+                dy = birdlist[j][1] - y  # y-distance between current i-th bird and j-th bird
+                dist = math.sqrt(dx * dx + dy * dy)  # euclidean distance
+                # if distance is smaller than minimum distance birds move away from each other
+                if (dist < min_dist):
+                    vx -= dx * 0.3
+                    vy -= dy * 0.3
+                # sum up velocities of nearby birds and counts them
+                if (dist < match_speed_window):
+                    avxtotal += birdlist[j][2]
+                    avytotal += birdlist[j][3]
+                    avcount += 1
+            j += 1
         # Match to average velocity of nearby birds
         if (avcount != 0):
             avx = avxtotal / avcount
@@ -232,6 +250,18 @@ while not quit_pressed:
                 vx *= 0.6 # Slow down
                 vy -= dy * 0.1
                 vy *= 0.6 # Slow down
+        # Birds avoid birds of other species
+        for one in range(len(birdlist)):
+            for two in range(len(birdlist2)):
+                dx2_dx1 = birdlist2[two][0] - birdlist[one][0]  # x-distance between current i-th bird and j-th bird
+                dy2_dy1 = birdlist2[two][1] - birdlist[one][1]  # y-distance between current i-th bird and j-th bird
+                dist2_1 = math.sqrt(dx2_dx1 * dx2_dx1 + dy2_dy1 * dy2_dy1)  # euclidean distance
+                # if distance is smaller than minimum distance birds move away from each other
+                if (dist2_1 < min_dist_between_species):
+                    vx -= dx * 0.8
+                    vx *= 40.0
+                    vy -= dy * 0.8
+                    vy *= 40.0
 
         # Birds bound off bounds of screen window
         if x >= width + 100:
@@ -267,6 +297,137 @@ while not quit_pressed:
         birdlist[i][2] = vx
         birdlist[i][3] = vy
         i += 1
+
+# -------------------------------------------------------------------------------------------
+# Birds2 section
+# -------------------------------------------------------------------------------------------
+    # Set leader bird to bird that has smallest distance to food
+    if food_closest2 is not None:
+        leaderbirdx2 = birdlist2[food_closest2][0]
+        leaderbirdy2 = birdlist2[food_closest2][1]
+        leaderbirdvx2 = birdlist2[food_closest2][2]
+        leaderbirdvy2 = birdlist2[food_closest2][3]
+
+    # Update leader bird position and speed
+    if (leaderbirdx2 < leader_border2):
+        leaderbirdvx2 += border_speed_change2
+    if (leaderbirdy2 < leader_border2):
+        leaderbirdvy2 += border_speed_change2
+    if (leaderbirdx2 > width - leader_border2):
+        leaderbirdvx2 -= border_speed_change2
+    if (leaderbirdy2 > height - leader_border2):
+        leaderbirdvy2 -= border_speed_change2
+
+    # Draw leaderbird and update
+    leaderbirdvx2 += random.uniform(-leader_random_speed_change2, leader_random_speed_change2)
+    leaderbirdvy2 += random.uniform(-leader_random_speed_change2, leader_random_speed_change2)
+    # Cap maximum speed of leader bird
+    speed2 = math.sqrt(leaderbirdvx2*leaderbirdvx2 + leaderbirdvy2*leaderbirdvy2)
+    if (speed2 > leader_max_speed2):
+        leaderbirdvx2 = leaderbirdvx2 * leader_max_speed2 / speed2
+        leaderbirdvy2 = leaderbirdvy2 * leader_max_speed2 / speed2
+    leaderbirdx2 += leaderbirdvx2
+    leaderbirdy2 += leaderbirdvy2
+    k = 0
+    min_food_dist2 = 1500
+    while (k < len(birdlist2)):
+        # Make copies for clarity
+        x2 = birdlist2[k][0]
+        y2 = birdlist2[k][1]
+        vx2 = birdlist2[k][2]
+        vy2 = birdlist2[k][3]
+        # Set colors of birds
+        colr2 = 0
+        colg2 = 255
+        colb2 = 255
+        # Draw birds
+        pygame.draw.circle(screen, (colr2, colg2, colb2), (int(x2), int(y2)), 6, 0)
+
+        # Birds move towards leader bird
+        leaderdiffx2 = leaderbirdx2 - x2
+        leaderdiffy2 = leaderbirdy2 - y2
+        vx2 += 0.008 * leaderdiffx2
+        vy2 += 0.008* leaderdiffy2
+
+        # Birds calculate distance to food
+        if food is not None:
+            food_dist2 = math.sqrt(math.pow(x2 - food[0], 2) + math.pow(y2 - food[1], 2))
+            if food_dist2 < min_food_dist2:
+                min_food_dist2 = food_dist2
+                food_closest2 = k
+        # Calculate average velocity of other birds
+        # Bird moves away from neighbours that are too close
+        j = 0
+        avxtotal2 = 0
+        avytotal2 = 0
+        avcount2 = 0
+        while (j < no_of_birds2):
+            if (j != k):
+                dx2 = birdlist2[j][0] - x2  # x-distance between current i-th bird and j-th bird
+                dy2 = birdlist2[j][1] - y2  # y-distance between current i-th bird and j-th bird
+                dist2 = math.sqrt(dx2 * dx2 + dy2 * dy2)  # euclidean distance
+                # if distance is smaller than minimum distance birds move away from each other
+                if (dist2 < min_dist2):
+                    vx2 -= dx2 * 0.3
+                    vy2 -= dy2 * 0.3
+                # sum up velocities of nearby birds and counts them
+                if (dist2 < match_speed_window2):
+                    avxtotal2 += birdlist2[j][2]
+                    avytotal2 += birdlist2[j][3]
+                    avcount2 += 1
+            j += 1
+        # Match to average velocity of nearby birds
+        if (avcount2 != 0):
+            avx2 = avxtotal2 / avcount2
+            avy2 = avytotal2 / avcount2
+            vx2 = 0.9 * vx2 + 0.1 * avx2  # Add x average velocity component to x-velocity
+            vy2 = 0.9 * vy2 + 0.1 * avy2  # Add y average velocity component to y-velocity
+
+        # Birds bounce off obstacles and slow down
+        for barrier in barriers:
+            dx2 = barrier[0] - x2
+            dy2 = barrier[1] - y2
+            dist2 = math.sqrt(dx2*dx2 + dy2*dy2)
+            if (dist2 < barrier_radius + 15):
+                vx2 -= dx2 * 0.1
+                vx2 *= 0.6 # Slow down
+                vy2 -= dy2 * 0.1
+                vy2 *= 0.6 # Slow down
+
+        # Birds bound off bounds of screen window
+        if x2 >= width + 100:
+            vx2 -= x2 * 0.1
+            vx2 *= 0.6  # Slow down
+            vy2 -= y2 * 0.1
+            vy2 *= 0.6  # Slow down
+        if y2 >= height + 100:
+            vx2 -= x2 * 0.1
+            vx2 *= 0.6  # Slow down
+            vy2 -= y2 * 0.1
+            vy *= 0.6  # Slow down
+        if x2 < -100:
+            vx2 -= x2 * 0.1
+            vx2 *= 0.6  # Slow down
+            vy2 -= y2 * 0.1
+            vy2 *= 0.6  # Slow down
+        if y2 < -100:
+            vx2 -= x2 * 0.1
+            vx2 *= 0.6  # Slow down
+            vy2 -= y2 * 0.1
+            vy2 *= 0.6  # Slow down
+
+        # Cap maximum speed
+        speed2 = math.sqrt(vx2*vx2 + vy2*vy2)
+        if (speed2 > max_speed2):
+            vx2 = vx2 * max_speed2 / speed2
+            vy2 = vy2 * max_speed2 / speed2
+
+        # Update positions according to speeds
+        birdlist2[k][0] += vx2
+        birdlist2[k][1] += vy2
+        birdlist2[k][2] = vx2
+        birdlist2[k][3] = vy2
+        k += 1
 
 # -------------------------------------------------------------------------------------------
 # App functionalities
